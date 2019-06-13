f4fc8b48c2a6550a473983371841d6a445d336d2
TARIK AIMAD <freelancetarik01@gmail.com>
2019-06-13 17:47:00 +0100

    Authentication&ListMovie

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/login.dart';
+
+
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Google Sign In',
+      home: LoginPage(),
+    ),
+
+  );
+}
